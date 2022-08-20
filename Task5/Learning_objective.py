fee6654fb25bf7794f006c7c28f0e7da3eaf8b3d
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-20 10:51:32 +0100

    training test

--- Task5/Learning_objective.py
+++ Task5/Learning_objective.py
@@ -9,15 +9,17 @@ import numpy as np
 from lambeq import BobcatParser
 from discopy import grammar
 from discopy import Dim
-from lambeq import AtomicType, SpiderAnsatz
-#from pytket.extensions.qiskit import AerBackend
+from lambeq import AtomicType, IQPAnsatz, remove_cups
+from pytket.extensions.qiskit import AerBackend
 from lambeq import TketModel
+from lambeq import QuantumTrainer, SPSAOptimizer
+from lambeq import Dataset
 
 BATCH_SIZE =42
 EPOCHS = 1000
 SEED = 2
 
-#input data
+#Input data
 def read_data(filename):
     labels, sentences = [], []
     with open(filename) as f:
@@ -29,26 +31,77 @@ def read_data(filename):
 
 
 train_labels, train_data = read_data('Task5/train_data.txt')
-#print(train_data[:8])
-#print(train_labels[:8])
 
 val_labels, val_data = read_data('Task5/val_data.txt')
-#print(val_data[:8])
-#print(val_labels[:8])
 
 
-#parser
+#Parser
 parser = BobcatParser(root_cats=('NP', 'N'), verbose='text')
 train_diagrams = parser.sentences2diagrams(train_data, suppress_exceptions=True)
+val_diagrams = parser.sentences2diagrams(val_data, suppress_exceptions=True)
 
 train_diagrams = [
     diagram.normal_form()
-    for diagram in train_diagrams if diagram is not None
-]
+    for diagram in train_diagrams if diagram is not None]
+val_diagrams = [
+    diagram.normal_form()
+    for diagram in val_diagrams if diagram is not None]
 
 train_labels = [
     label for (diagram, label)
     in zip(train_diagrams, train_labels)
     if diagram is not None]
+val_labels = [
+    label for (diagram, label)
+    in zip(val_diagrams, val_labels)
+    if diagram is not None]
 
-#train_diagrams[0].draw(figsize=(9, 5), fontsize=12)
+#Circuits
+ansatz = IQPAnsatz({AtomicType.NOUN: 1, AtomicType.SENTENCE: 0},
+                   n_layers=1, n_single_qubit_params=3)
+
+train_circuits = [ansatz(remove_cups(diagram)) for diagram in train_diagrams]
+val_circuits =  [ansatz(remove_cups(diagram))  for diagram in val_diagrams]
+
+#train_circuits[0].draw(figsize=(9, 10))
+
+#Model
+all_circuits = train_circuits + val_circuits
+
+backend = AerBackend()
+backend_config = {
+    'backend': backend,
+    'compilation': backend.default_compilation_pass(2),
+    'shots': 8192
+}
+
+model = TketModel.from_diagrams(all_circuits, backend_config=backend_config)
+
+#Loss & eval metrics
+loss = lambda y_hat, y: -np.sum(y * np.log(y_hat)) / len(y)  # binary cross-entropy loss
+acc = lambda y_hat, y: np.sum(np.round(y_hat) == y) / len(y) / 2  # half due to double-counting
+eval_metrics = {"acc": acc}
+
+#Trainer
+trainer = QuantumTrainer(
+    model,
+    loss_function=loss,
+    epochs=EPOCHS,
+    optimizer=SPSAOptimizer,
+    optim_hyperparams={'a': 0.05, 'c': 0.06, 'A':0.01*EPOCHS},
+    evaluate_functions=eval_metrics,
+    evaluate_on_train=True,
+    verbose = 'text',
+    seed=0
+)
+
+#Create dataset
+train_dataset = Dataset(
+            train_circuits,
+            train_labels,
+            batch_size=BATCH_SIZE)
+
+val_dataset = Dataset(val_circuits, val_labels, shuffle=False)
+
+#Train model
+trainer.fit(train_dataset, val_dataset, evaluation_step=1, logging_step=100)
